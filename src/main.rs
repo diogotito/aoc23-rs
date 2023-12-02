65508644c3147eb0b867ae1e50ccd0a8f97642da
diogotito <diogotitomarques@gmail.com>
2023-12-02 16:40:11 +0000

    day 1 part 2

--- src/main.rs
+++ src/main.rs
@@ -8,22 +8,61 @@ fn main() -> anyhow::Result<()> {
 }
 
 fn day1() -> anyhow::Result<()> {
-    println!("day 1 part 1 answer: {}", day1_part1(puzzle_input_lines("input/day1.txt")?));
-    println!("day 1 part 2 answer: {}", day1_part2(puzzle_input_lines("input/day1.txt")?));
+    println!(
+        "day 1 part 1 answer: {}",
+        day1_part1(puzzle_input_lines("input/day1.txt")?)
+    );
+    println!(
+        "day 1 part 2 answer: {}",
+        day1_part2(puzzle_input_lines("input/day1.txt")?)
+    );
     Ok(())
 }
 
 fn day1_part1(input: impl Iterator<Item = String>) -> u64 {
-    input.map(|line| {
-        let is_digit = |c: &char| c.is_ascii_digit();
-        let first_digit = line.chars().find(is_digit).unwrap() as u64 - '0' as u64;
-        let last_digit = line.chars().rfind(is_digit).unwrap() as u64 - '0' as u64;
-        10 * first_digit + last_digit
-    }).sum()
+    input
+        .map(|line| {
+            let is_digit = |c: &char| c.is_ascii_digit();
+            let left_digit = line.chars().find(is_digit).unwrap() as u64 - '0' as u64;
+            let right_digit = line.chars().rfind(is_digit).unwrap() as u64 - '0' as u64;
+            10 * left_digit + right_digit
+        })
+        .sum()
 }
 
-fn day1_part2(input: impl Iterator<Item = String>) -> i32 {
-    input.count() as i32
+fn day1_part2(input: impl Iterator<Item = String>) -> u64 {
+    const DIGIT_NAMES_CHARS_AND_VALUES: [(&str, char, u64); 9] = [
+        ("one", '1', 1),
+        ("two", '2', 2),
+        ("three", '3', 3),
+        ("four", '4', 4),
+        ("five", '5', 5),
+        ("six", '6', 6),
+        ("seven", '7', 7),
+        ("eight", '8', 8),
+        ("nine", '9', 9),
+    ];
+    input
+        .map(|line| {
+            let (_, _, left_digit) = DIGIT_NAMES_CHARS_AND_VALUES
+                .iter()
+                .min_by_key(|(name, char, _)| {
+                    line.find(name)
+                        .unwrap_or(usize::MAX)
+                        .min(line.find(*char).unwrap_or(usize::MAX))
+                })
+                .unwrap();
+            let (_, _, right_digit) = DIGIT_NAMES_CHARS_AND_VALUES
+                .iter()
+                .max_by_key(|(name, char, _)| {
+                    let i32_score =
+                        |o: Option<usize>| o.map(|i: usize| i as i32).unwrap_or(i32::MIN);
+                    i32_score(line.rfind(name)).max(i32_score(line.rfind(*char)))
+                })
+                .unwrap();
+            10 * left_digit + right_digit
+        })
+        .sum()
 }
 
 fn puzzle_input_lines(p: impl AsRef<Path>) -> io::Result<impl Iterator<Item = String>> {
@@ -34,12 +73,13 @@ fn puzzle_input_lines(p: impl AsRef<Path>) -> io::Result<impl Iterator<Item = St
 
 #[cfg(test)]
 mod tests {
-    use std::io::Lines;
-    use std::iter::MapWhile;
     use super::*;
     use rstest::*;
+    use std::io::Lines;
+    use std::iter::MapWhile;
 
-    type InputLinesIterator = MapWhile<Lines<BufReader<File>>, fn(io::Result<String>) -> Option<String>>;
+    type InputLinesIterator =
+        MapWhile<Lines<BufReader<File>>, fn(io::Result<String>) -> Option<String>>;
 
     fn input_lines_for_test(input_file_path: &Path) -> InputLinesIterator {
         BufReader::new(File::open(input_file_path).unwrap())
@@ -49,11 +89,15 @@ mod tests {
 
     #[rstest]
     #[case(day1_part1, "input/day1.txt", 55108)]
+    #[case(day1_part2, "input/day1.txt", 56324)]
     fn check_answers(
         #[case] part_function: fn(InputLinesIterator) -> u64,
         #[case] puzzle_input: impl AsRef<Path>,
-        #[case] correct_answer: u64
+        #[case] correct_answer: u64,
     ) {
-        assert_eq!(part_function(input_lines_for_test(puzzle_input.as_ref())), correct_answer);
+        assert_eq!(
+            part_function(input_lines_for_test(puzzle_input.as_ref())),
+            correct_answer
+        );
     }
 }
